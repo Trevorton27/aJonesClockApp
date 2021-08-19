202a171aa8564f1b8b8b868e71631fc04bdf586d
Trevor Mearns <spiral272@gmail.com>
2021-08-19 12:14:24 +0900

    ready for code review approval

--- script.js
+++ script.js
@@ -1,98 +1,84 @@
-// 	hour = hour < 10 ? "0" + hour : hour;
-// 	min = min < 10 ? "0" + min : min;
-// 	sec = sec < 10 ? "0" + sec : sec;
-
-// 	const showTime = hour + ":"
-// 			+ min + ":" + sec + am_pm;
-
-// 	document.getElementById("digital_Clock")
-// 			.innerHTML = currentTime;
-
-//     const monthOfTheYear = time.getMonth("January","Febuary","March","April","May","June","July","August","September","October","November","December");
-//     const dayOfTheWeek = time.getDay("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
-// }
-// showTime();
-
-function currentClock() {
+function renderClock() {
   const showTime = new Date();
 
-  /* time */
-  var hr,
-    _min =
-      showTime.getMinutes() < 10
-        ? "0" + showTime.getMinutes()
-        : showTime.getMinutes(),
-    sec =
-      showTime.getSeconds() < 10
-        ? "0" + showTime.getSeconds()
-        : showTime.getSeconds(),
-    ampm = showTime.getHours() >= 12 ? "PM" : "AM";
+  let hours = showTime.getHours();
+  const minutes = renderLeadingZero(showTime.getMinutes());
+  const seconds = renderLeadingZero(showTime.getSeconds());
+  const isAm = hours < 12 || hours === 0;
+  const amPm = isAm ? 'AM' : 'PM';
 
-  // 12 will be subtracted from any time designated from 13 to 23 and midnight will start at 0
-  if (showTime.getHours() == 0) {
-    hr = 12;
-  } else if (showTime.getHours() > 12) {
-    hr = showTime.getHours() - 12;
-  } else if (showTime.getHours() < 10) {
-    hr = "0" + showTime.getHours();
-  } else {
-    hr = showTime.getHours();
-  }
-
-  //   current time holds results for hr min sec untill it is applied to the dom by the getElementBy ID
-
-  const currentTime = hr + ":" + _min + ":" + sec;
-
-  // AM/ PM options
-
-  document.getElementById("time").innerHTML = currentTime;
-  document.getElementById("tod").innerHTML = ampm;
-  //   print options to display to the  date and time to the DOM
-
-  /*  Add Date options */
-  /* dow Day of the week*/
-  //    moty Month of the year
-  const dotw = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  //   const dotw = showTime.getDay("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
-
-  const moty = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-
-  // const moty = showTime.getMonth("January","Febuary","March","April","May","June","July","August","September","October","November","December");
-
-  day = showTime.getDate();
-
-  // store date
-  var currentDate =
-    dotw[showTime.getDay()] + ", " + moty[showTime.getMonth()] + " " + day;
-
-  // var currentDate = dotw[] + ", " + moty[] + " " + day;
-
-  document.getElementById("date").innerHTML = currentDate;
+  document.getElementById('time').textContent = `${formatHour(
+    hours
+  )}:${minutes}:${seconds} ${amPm}`;
 }
 
-// print time and date once, then update them every second
-currentClock();
-setInterval(function () {
-  currentClock();
+function renderLeadingZero(number) {
+  return number < 10 ? '0' + number : number;
+}
+
+function formatHour(hour) {
+  hour = hour >= 13 ? hour - 12 : hour;
+  hour = hour === 0 ? hour + 12 : hour;
+
+  return hour;
+}
+
+function renderDate() {
+  const showDate = new Date();
+
+  const day = dayOfWeek[showDate.getDay()];
+  const date = appendDateSuffix(showDate.getDate());
+  const month = monthOfTheYear[showDate.getMonth()];
+  const year = showDate.getFullYear();
+
+  document.getElementById(
+    'date'
+  ).textContent = `${day}, ${month} ${date}, ${year}`;
+}
+
+function appendDateSuffix(date) {
+  if (date < 10 || date > 20) {
+    switch (date % 10) {
+      case 1:
+        return `${date}st`;
+      case 2:
+        return `${date}nd`;
+      case 3:
+        return `${date}rd`;
+    }
+  }
+  return `${date}th`;
+}
+
+const dayOfWeek = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+];
+
+const monthOfTheYear = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+
+renderClock();
+renderDate();
+
+setInterval(() => {
+  renderClock();
+  renderDate();
 }, 1000);
